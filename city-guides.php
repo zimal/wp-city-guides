6243500e5c97fe7c959eca208de0b103eeaa4034
Luboš Zíma <lubos.zima@gmail.com>
2016-06-26 22:02:29 +0000

    Adding plugin header

---
+++ city-guides.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+* Plugin Name: City Guides
+* Plugin URI: https://github.com/zimal/wp-city-guides
+* Description: Plugin pro správu ECT City Guides
+* Version: 0.1
+* Author: Luboš Zíma
+* Author URI: http://luboszima.cz
+* Text Domain: city-guidess
+* License: GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
